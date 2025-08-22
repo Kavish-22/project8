68852e07d125b636bcc7631c320d8777e0a0afc0
Kavish-22 <Kavishchouhan22@gmail.com>
2025-08-22 17:06:48 +0530

    new

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+//1. Import area 
+import './App.css'
+
+
+//2 Function defination area 
+
+function App() {
+
+  //2.1 Hooks area 
+
+  return (
+    <>
+    <div className='row'>
+      <div className='col-3'>
+        <input className="form-control" type="text" />
+      </div>
+      <div className='col-3'>
+        <input className="form-control" type="text" />
+      </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
